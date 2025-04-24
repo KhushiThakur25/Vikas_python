b569f7161cb4829ccb1618006419b11020385d7c
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2025-04-24 17:50:21 +0530

    Add files via upload

---
+++ All_Fundamentals.py
@@ -0,0 +1,221 @@
+Python 3.13.0 (tags/v3.13.0:60403a5, Oct  7 2024, 09:38:07) [MSC v.1941 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================= RESTART: E:/Vikas_Python/operators.py ================
+world
+>>> st = "Hello world"
+>>> 'r' in st
+True
+>>> 'n' not in st
+True
+>>> a  = 5
+>>> b = 5
+>>> a is b
+True
+>>> a = [2,3,4]
+>>> b = [2,3,4]
+>>> a is b
+False
+>>> n = a
+>>> n is a
+True
+n
+[2, 3, 4]
+a
+[2, 3, 4]
+n = a is b
+n
+False
+10<<1
+20
+10>>1
+5
+10&6
+2
+10|6
+14
+10^6
+12
+
+================= RESTART: E:/Vikas_Python/operators.py ================
+world
+
+================= RESTART: E:/Vikas_Python/operators.py ================
+world
+      gvdeshf
+      jhbdjksedjdc
+
+================= RESTART: E:/Vikas_Python/operators.py ================
+world
+      gvdeshf
+      jhbdjksedjdc
+Sum of a and b is 60
+
+================= RESTART: E:/Vikas_Python/operators.py ================
+world
+      gvdeshf
+      jhbdjksedjdc
+Sum of a and b is 60
+Sum of 56 and 4 is 60
+
+================= RESTART: E:/Vikas_Python/operators.py ================
+world
+      gvdeshf
+      jhbdjksedjdc
+Sum of a and b is 60
+Sum of 56 and 4 is 60
+Sum of 56 and 4 is 60
+
+================= RESTART: E:/Vikas_Python/operators.py ================
+world
+      gvdeshf
+      jhbdjksedjdc
+Sum of a and b is 60
+Sum of 56 and 4 is 60
+Sum of 56 and 4 is 60
+Sum of {a} and {b} is {a+b}
+
+================= RESTART: E:/Vikas_Python/operators.py ================
+world
+      gvdeshf
+      jhbdjksedjdc
+Sum of a and b is 60
+Sum of 56 and 4 is 60
+Sum of 56 and 4 is 60
+Sum of 56 and 4 is 60
+
+================= RESTART: E:/Vikas_Python/operators.py ================
+world
+      gvdeshf
+      jhbdjksedjdc
+Sum of a and b is 60
+Sum of 56 and 4 is 60
+Sum of 56 and 4 is 60
+Sum of 56 and 4 is 60
+Sum of 56 and 4 is 60
+
+================= RESTART: E:/Vikas_Python/operators.py ================
+world
+      gvdeshf
+      jhbdjksedjdc
+Sum of a and b is 61
+Sum of 57 and 4 is 61
+Sum of 57 and 4 is 61
+Sum of 57 and 4 is 14
+Sum of 57 and 4 is 61
+
+================= RESTART: E:/Vikas_Python/operators.py ================
+world
+      gvdeshf
+      jhbdjksedjdc
+Sum of a and b is 61
+Sum of 57 and 4 is 61
+Sum of 57 and 4 is 61
+Sum of 57 and 4 is 14.250000
+Sum of 57 and 4 is 61
+a = input("Enter your value")
+Enter your value56
+b = input("Enter your next value")
+Enter your next value4
+a + b
+'564'
+a = int(input("Enter your value"))
+Enter your value56
+b = int(input("Enter your value"))
+Enter your value4
+a+b
+60
+"Hello " + "World"
+'Hello World'
+5 + 6
+11
+n = 56
+if n > 50:
+    print("Grade A")
+else:
+    print("Grade B")
+
+Grade A
+1900%400
+300
+n = 596
+m = 154
+\
+n
+596
+m
+154
+if n > m:
+    print("n is greater")
+else:
+    print("m is greater")
+
+n is greater
+p = 52
+if n > m and n > p:
+    print("n is greatest")
+elif m > n and m >p:
+    print("m is greatest")
+else:
+    print("p is greatest")
+
+n is greatest
+n = int(input("Enter the value of n:"))
+Enter the value of n:-56
+if n < 0:
+    print("Number is negative..")
+elif n > 0:
+    print("Number is positive..")
+else:
+    print("Number is zero")
+
+Number is negative..
+n =  56
+if n%2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+Even
+n = 110
+if n%5 == 0 and n%11 == 0:
+    print("Number is Lucky")
+else:
+    print("Number is not Lucky")
+
+Number is Lucky
+year = 1980
+if year%4 == 0 and year%100 != 0:
+    print("Year is leap year")
+else:
+    print("Year is not leap year")
+
+Year is leap year
+
+================= RESTART: E:/Vikas_Python/operators.py ================
+Enter your physics marks88
+Enter your chemistry marks94
+Enter your maths marks93
+Enter your biology marks83
+Enter your computer marks91
+Grade B
+
+================= RESTART: E:/Vikas_Python/operators.py ================
+Enter your physics marks88
+Enter your chemistry marks94
+Enter your maths marks93
+Enter your biology marks83
+Enter your computer marks91
+Your percentage is 89.8
+Grade B
+
+================= RESTART: E:/Vikas_Python/operators.py ================
+Enter your basic salary..10000
+Traceback (most recent call last):
+  File "E:/Vikas_Python/operators.py", line 47, in <module>
+    HRA = 0.20 * BS
+NameError: name 'BS' is not defined
+
+================= RESTART: E:/Vikas_Python/operators.py ================
+Enter your basic salary..10000
+Your gross salary is: 20000.0
